fc2e66538d761828d1ebff86a4ed4d02197296fc
Anshuraj09 <anshuraj2771@gmail.com>
2024-10-20 11:41:14 +0530

    updated

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
@@ -8,17 +9,16 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      {/* Enhanced lighting for a brighter scene */}
-      <hemisphereLight intensity={0.5} groundColor='white' /> {/* Brighter ambient light */}
+      <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={2} // Increased intensity for more brightness
+        intensity={1}
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1.5} color="#FFD700" /> {/* Yellow point light for warmth */}
+      <pointLight intensity={1} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
@@ -33,15 +33,21 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
+    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
+    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
-    
+
+    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
